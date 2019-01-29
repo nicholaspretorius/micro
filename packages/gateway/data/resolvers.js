aa1006b86e21b0570065f8d3f27f2fb0d7586306
Nicholas Pretorius <nicholaspretorius@gmail.com>
2019-01-29 20:02:03 +0200

    Moving towards mutations.

--- packages/gateway/data/resolvers.js
+++ packages/gateway/data/resolvers.js
@@ -1,6 +1,28 @@
+const mockMails = [
+  {
+    subject: "Hello world 1!",
+    receiver: "nic1@test.com",
+    content: "Hello Nic1!",
+    _id: "1"
+  },
+  {
+    subject: "Hello world 2!",
+    receiver: "nic2@test.com",
+    content: "Hello Nic2!",
+    _id: "2"
+  },
+  {
+    subject: "Hello world 3!",
+    receiver: "nic3@test.com",
+    content: "Hello Nic3!",
+    _id: "3"
+  }
+];
+
 const resolvers = {
   Query: {
-    status: () => "Hello world!"
+    mails: () => mockMails,
+    mail: (_, args, context) => console.log(args, context)
   }
 };
 
--- packages/gateway/data/types.js
+++ packages/gateway/data/types.js
@@ -3,13 +3,22 @@ const { gql } = require("apollo-server-express");
 const Query = gql`
   type Query {
     status: String!
+    mails: [Mail]
+    mail(subject: String!, receiver: String!): Mail
+  }
+
+  type Mail {
+    subject: String
+    receiver: String
+    content: String
+    _id: String
   }
 `;
 
-const Mutation = `
-    type Mutation {
-        _empty: String
-    }
+const Mutation = gql`
+  type Mutation {
+    mail(subject: String!, receiver: String!, content: String!): Mail
+  }
 `;
 
 const typeDefs = [Query, Mutation];
