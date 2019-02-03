dd794d4b07e4234c7410b0f883eb88468b01717e
Nicholas Pretorius <nicholaspretorius@gmail.com>
2019-02-03 12:41:16 +0200

    Added test mails.

--- packages/db-service/routes/get.js
+++ packages/db-service/routes/get.js
@@ -1,5 +1,21 @@
+const mongoose = require("mongoose");
+const Mail = require("./../models/mail");
+
 module.exports = server => {
-  server.get("/", (_, res) => {
-    res.send("Hello Database");
+  server.get("/", async (_, res) => {
+    const mail = new Mail({
+      subject: "First Email",
+      receiver: "nicholaspre@icloud.com",
+      content: "Hello Nicholas!"
+    });
+
+    await mail.save();
+
+    res.json(mail);
+  });
+
+  server.get("/mails", async (_, res) => {
+    const mails = await Mail.find();
+    res.json(mails);
   });
 };
